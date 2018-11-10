0e04947b7360f8c3799b3f41a85fb5a4637ae5dd
Charlotte Dunois <github@charuru.moe>
2018-11-10 22:28:00 +0100

    Use preg replace callback

--- src/Utility.php
+++ src/Utility.php
@@ -18,22 +18,21 @@ class Utility {
      * The callable is used to return numbered parameters (such as used in PostgreSQL), or any other kind of parameters supported by the DBMS.
      * @param string                $query
      * @param string|callable|null  $replaceParams  If `null` is passed, it will not replace the parameters.
-     * @return array  `[ 'query' => string, 'parameters' => array ]`  The `parameters` array is an numeric array (= position), which mappes to the parameter.
+     * @return array  `[ 'query' => string, 'parameters' => array ]`  The `parameters` array is an numeric array (= position, starting at 1), which map to the original parameter.
      */
     static function parseParameters(string $query, $replaceParams = '?', string $regex = '/(:[a-z]+)|\?|\$\d+/i'): array {
         $params = array();
-        
         $position = 1;
-        \preg_match_all($regex, $query, $matches);
         
-        foreach($matches[0] as $match) {
+        $query = \preg_replace_callback($regex, function (array $match) use ($replaceParams, &$params, &$position) {
+            $params[($position++)] = $match[0];
+            
             if($replaceParams !== null) {
-                $replacement = (\is_callable($replaceParams) ? $replaceParams() : $replaceParams);
-                $query = \preg_replace('/'.preg_quote($match, '/').'/', $replacement, $query, 1);
+                return (\is_callable($replaceParams) ? $replaceParams() : $replaceParams);
             }
             
-            $params[($position++)] = $match;
-        }
+            return $match[0];
+        }, $query);
         
         return array('query' => $query, 'parameters' => $params);
     }
