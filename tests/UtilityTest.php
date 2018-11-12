50a035c9481ab0f9c458ad9f880052180724610f
Charlotte Dunois <github@charuru.moe>
2018-11-12 18:47:38 +0100

    Add utility test

---
+++ tests/UtilityTest.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Plasma Core component
+ * Copyright 2018 PlasmaPHP, All Rights Reserved
+ *
+ * Website: https://github.com/PlasmaPHP
+ * License: https://github.com/PlasmaPHP/core/blob/master/LICENSE
+*/
+
+namespace Plasma\Tests;
+
+class UtilityTest extends TestCase {
+    function testParseParameters() {
+        [ 'query' => $query, 'parameters' => $params] = \Plasma\Utility::parseParameters(
+            'SELECT `a` FROM `HERE_WE_GO` WHERE a < ? AND b = :named OR  c = $1',
+            '?'
+        );
+        
+        $this->assertSame('SELECT `a` FROM `HERE_WE_GO` WHERE a < ? AND b = ? OR  c = ?', $query);
+        $this->assertSame(array(
+            1 => '?',
+            2 => ':named',
+            3 => '$1'
+        ), $params);
+    }
+    
+    function testParseParametersCallback() {
+        [ 'query' => $query, 'parameters' => $params] = \Plasma\Utility::parseParameters(
+            'SELECT `a` FROM `HERE_WE_GO` WHERE a < :na AND b = $5 OR  c = ?',
+            function () {
+                static $i;
+                
+                if(!$i) {
+                    $i = 0;
+                }
+                
+                return '$'.(++$i);
+            }
+        );
+        
+        $this->assertSame('SELECT `a` FROM `HERE_WE_GO` WHERE a < $1 AND b = $2 OR  c = $3', $query);
+        $this->assertSame(array(
+            1 => ':na',
+            2 => '$5',
+            3 => '?'
+        ), $params);
+    }
+}
