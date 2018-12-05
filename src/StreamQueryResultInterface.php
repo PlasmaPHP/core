523bd74ca1809c36d0e9c2accbfe940477587d3e
Charlotte Dunois <github@charuru.moe>
2018-12-05 09:33:22 +0100

    Update comment [ci skip

--- src/StreamQueryResultInterface.php
+++ src/StreamQueryResultInterface.php
@@ -17,6 +17,9 @@ namespace Plasma;
 interface StreamQueryResultInterface extends \React\Stream\ReadableStreamInterface, QueryResultInterface {
     /**
      * Buffers all rows and returns a promise which resolves with an instance of `QueryResultInterface`.
+     * This method does not guarantee that all rows get returned, as the buffering depends on when this
+     * method gets invoked. As such implementations may buffer rows directly from the start to ensure
+     * all rows get returned. But users must not assume this behaviour is the case.
      * @return \React\Promise\PromiseInterface
      */
     function all(): \React\Promise\PromiseInterface;
